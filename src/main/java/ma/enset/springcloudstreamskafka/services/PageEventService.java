b0dd2f0bc82d207190ad48befd135c8c9d4592bc
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-09 19:40:48 +0100

    pageEventSupplier

--- src/main/java/ma/enset/springcloudstreamskafka/services/PageEventService.java
+++ src/main/java/ma/enset/springcloudstreamskafka/services/PageEventService.java
@@ -4,7 +4,10 @@ import ma.enset.springcloudstreamskafka.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Service
 public class PageEventService {
@@ -15,5 +18,14 @@ public class PageEventService {
             System.out.println(input.toString());
             System.out.println("*************************");
         };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier() {
+        return () -> new PageEvent(
+                Math.random() < 0.5 ? "P1" : "P2",
+                Math.random() > 0.5 ? "U1" : "U2",
+                new Date(),
+                new Random().nextInt(9000));
     }
 }
